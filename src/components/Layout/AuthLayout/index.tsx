47c14796b66051211e8c498ea157a4437da99c0d
gunner714 <kanonier714@gmail.com>
2022-03-02 23:12:29 +0100

    Add files via upload

---
+++ src/components/Layout/AuthLayout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AuthPage from "../../../pages/AuthPage";
+import { Switch, Route } from "react-router-dom";
+import classes from "./AuthLayout.module.css";
+import Box from "@mui/material/Box";
+
+const AuthLayout = () => {
+  return (
+    <Box className={classes.container}>
+      <Switch>
+        <Route exact path="/" render={() => <AuthPage />}></Route>
+      </Switch>
+    </Box>
+  );
+};
+
+export default AuthLayout;
